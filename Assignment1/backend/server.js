a9fb8302998bc3de195d3513b0a2ddc073e5b419
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2022-04-03 19:25:51 +0530

    Update server.js

--- Assignment1/backend/server.js
+++ Assignment1/backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connection to MongoDB
 // mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true });
-mongoose.connect('mongodb+srv://vijayreddy:test123@tradingapi.mwygi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://<Anan>:<Anantha123>@cluster0.qdufw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully !");
